d14cfa3950a2205b0b4610e5cd1437415d42674e
lukasblois <lukasblois.mail@gmail.com>
2026-03-02 13:11:10 -0600

    seeing if anything changed

--- app/config.py
+++ app/config.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
     allow_origins: List[str] = ["https://localhost:8000",
+                                "http://localhost:8000",
                                 "https://fastapi-l3f5.onrender.com/"]
     limit_global: str
     limit_users_create: str
